7e2e5732808cea4172af4ebe3400619bec2cbd06
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-05-14 00:26:27 +0700

    testing

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -4,8 +4,8 @@ module.exports = {
 
   // This will lint and format TypeScript and                                             //JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `npx eslint --fix ${filenames.join(" ")}`,
     `npx prettier --write ${filenames.join(" ")}`,
+    `npx eslint --fix ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
