a21a03693d5d5c0fcf01e2c9e4346ad73e65484b
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-05-27 01:58:51 +0700

    temp color

--- frontend/components/navbar/NavBar.tsx
+++ frontend/components/navbar/NavBar.tsx
@@ -12,7 +12,7 @@ export default function Category() {
   }
 
   return (
-    <Disclosure as="nav" className="bg-sky-400">
+    <Disclosure as="nav" className="bg-blue-700">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
