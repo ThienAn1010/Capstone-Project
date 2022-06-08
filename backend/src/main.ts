7c1a1cf32365d0f8bd406130d4dcca9a4f80b090
Vu Thang <dangvuthang97@gmail.com>
2022-06-08 22:15:26 +0700

    use process.env to handle cookie config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
   app.use(cookieParser());
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({
+    origin: configService.get('CLIENT_URL'),
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(8000);
 }
 bootstrap();
