7b5856f71df909d25ff5fb5f6bc781af6763c7c2
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-06-10 21:13:41 +0700

    update categories

--- frontend/components/Category.tsx
+++ frontend/components/Category.tsx
@@ -19,17 +19,17 @@ export default function Category() {
     <div className="flex flex-wrap justify-around px-2 lg:px-32 mt-10 ">
       {categories.map((category) => (
         <React.Fragment key={category.id}>
-          <div className="hidden sm:inline-block w-52 flex-none group cursor-pointer mb-5 lg:inline-block w-80 flex-none group cursor-pointer mb-5">
-            <div className="w-44 h-44 lg:h-64 lg:w-64">
+          <div className="hidden sm:inline-block w-52 flex-none group cursor-pointer mb-5 lg:inline-block lg:w-80 flex-none group cursor-pointer mb-5">
+            <div className="w-34 h-34 lg:w-56 lg:h-56">
               <img
                 src={category.src}
                 className="object-cover border rounded-md h-full w-full mix-blend-multiply lg:transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
                 alt="category"
               />
             </div>
-            <div className="mt-2">
+            {/* <div className="mt-2">
               <h1 className="font-bold">{category.name}</h1>
-            </div>
+            </div> */}
           </div>
           <div className="flex-initial mt-2 mb-2 sm:hidden">
             <h1 className="rounded-full border py-1 px-2 mr-1 font-bold border-black hover:bg-gray-200 flex items-center justify-center text-center">
