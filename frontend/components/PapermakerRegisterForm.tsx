e6fce4625f41962c0fd5023f0d886cb463bd1ff7
NguyenDangLamPhuong <67723724+NguyenDangLamPhuong@users.noreply.github.com>
2022-06-18 22:50:45 +0700

    upload photo

--- frontend/components/PapermakerRegisterForm.tsx
+++ frontend/components/PapermakerRegisterForm.tsx
@@ -94,7 +94,6 @@ const PapermakerRegisterForm = () => {
                     />
                   </div>
                 </div>
-                <div></div>
                 <div className="space-y-1">
                   <label
                     htmlFor="password"
@@ -129,6 +128,28 @@ const PapermakerRegisterForm = () => {
                       required
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
+                  </div>
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Photo
+                  </label>
+                  <div className="mt-1 flex items-center">
+                    <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                      <svg
+                        className="h-full w-full text-gray-300"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                      </svg>
+                    </span>
+                    <button
+                      type="button"
+                      className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Upload
+                    </button>
                   </div>
                 </div>
                 <div>
