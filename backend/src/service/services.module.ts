e7ce6d701a6a75f7928d025aafbe44eae7aea496
Vu Thang <dangvuthang97@gmail.com>
2022-06-18 22:53:28 +0700

    done sort| ui for filter and pagination

--- backend/src/service/services.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ServicesController } from './services.controller';
-import { ServicesService } from './services.service';
-
-@Module({
-  controllers: [ServicesController],
-  providers: [ServicesService],
-})
-export class ServicesModule {}
