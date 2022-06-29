4668a55bec4b64f41742656ab318c28376a8ba95
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-06-29 20:40:04 +0700

    update dropdown

--- frontend/components/navbar/NavBarLog.tsx
+++ frontend/components/navbar/NavBarLog.tsx
@@ -197,6 +197,41 @@ export default function NavBarLog({ data }: any) {
                                       : "text-gray-900",
                                     "flex justify-start space-x-2 items-center px-4 py-4 hover:cursor-pointer"
                                   )}
+                                >
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                                    />
+                                  </svg>
+                                  <a
+                                    href="#"
+                                    className="block text-md font-medium"
+                                  >
+                                    My Order
+                                  </a>
+                                </div>
+                              )}
+                            </Menu.Item>
+                          </div>
+                          <div>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <div
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 text-black"
+                                      : "text-gray-900",
+                                    "flex justify-start space-x-2 items-center px-4 py-4 hover:cursor-pointer"
+                                  )}
                                   onClick={handleLogout}
                                 >
                                   <svg
