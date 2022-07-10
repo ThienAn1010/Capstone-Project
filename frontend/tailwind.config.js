fef4052a177a648333e3dda559040810eecb8ea5
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-07-10 12:00:13 +0700

    fix deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,5 +10,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp"), require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/line-clamp"), require("@tailwindcss/forms")],
 }
