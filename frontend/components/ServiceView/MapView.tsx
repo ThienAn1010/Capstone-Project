aa5ce7000ce1dc2c540ff09c133c0a1ad67ec25b
dangvuthang <dangvuthang97@gmail.com>
2022-08-06 09:19:42 +0700

    add Marker

--- frontend/components/ServiceView/MapView.tsx
+++ frontend/components/ServiceView/MapView.tsx
@@ -1,9 +1,9 @@
 import { ClockIcon, TemplateIcon } from "@heroicons/react/outline"
 import { FC, useState } from "react"
-import ReactMapGL from "react-map-gl"
+import ReactMapGL, { Marker } from "react-map-gl"
 import { OfferedService } from "../../types/OfferedService"
 import Star from "../Star"
-
+import "mapbox-gl/dist/mapbox-gl.css"
 interface MapViewProps {
   setMapView: () => void
   offeredServicesData: {
@@ -42,37 +42,50 @@ const MapView: FC<MapViewProps> = ({
           />
         </div>
       </div>
-      <div className="w-1/4 absolute top-[60px] h-[calc(100%-60px)] left-0 z-10 p-5 shadow-md bg-[#efefef]">
-        <div className="bg-white flex overflow-hidden rounded-sm">
-          <div>
-            <img src={offeredServices[0].paperMaker.user.picture} alt="Image" />
-          </div>
-          <div className="px-4 py-2 flex-1">
-            <h3 className="font-medium text-sm">
-              {offeredServices[0].paperMaker.user.name}
-            </h3>
-            <div className="flex items-center">
-              <Star rating={offeredServices[0].paperMaker.rating} />
-              <span className="flex text-xs text-[#999]">
-                ({offeredServices[0].paperMaker.rating})
-              </span>
+      <div className="w-1/4 absolute top-[60px] h-[calc(100%-60px)] left-0 z-10 p-5 shadow-md bg-[#efefef] overflow-auto">
+        <div className="flex flex-col gap-y-5 ">
+          {offeredServices.map((offeredService) => (
+            <div
+              key={offeredService.id}
+              className="bg-white flex overflow-hidden rounded-md "
+            >
+              <div className="">
+                <img
+                  width="100%"
+                  height="100%"
+                  src={offeredService.paperMaker.user.picture}
+                  alt="Image"
+                  className="object-contain"
+                />
+              </div>
+              <div className="px-4 py-2 flex-2">
+                <h3 className="font-medium text-sm">
+                  {offeredService.paperMaker.user.name}
+                </h3>
+                <div className="flex items-center">
+                  <Star rating={offeredService.paperMaker.rating} />
+                  <span className="flex text-xs text-[#999]">
+                    ({offeredService.paperMaker.rating})
+                  </span>
+                </div>
+                <div className="flex mt-2 items-center gap-x-3">
+                  <p className="text-xs text-[#999]">
+                    <span className="font-semibold text-base text-gray-900">
+                      ${offeredService.price}
+                    </span>
+                    / service
+                  </p>
+                  <p className="flex items-baseline gap-x-0.5 text-xs text-[#999]">
+                    <ClockIcon className="w-5 h-5 text-gray-900 font-semibold flex self-center" />
+                    <span className="flex font-semibold text-base text-gray-900">
+                      {offeredService.duration}
+                    </span>
+                    days
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="flex mt-2 items-center justify-between">
-              <p className="text-xs text-[#999]">
-                <span className="font-semibold text-base text-gray-900">
-                  ${offeredServices[0].price}
-                </span>
-                / service
-              </p>
-              <p className="flex items-baseline gap-x-0.5 text-xs text-[#999]">
-                <ClockIcon className="w-5 h-5 text-gray-900 font-semibold flex self-center" />
-                <span className="flex font-semibold text-base text-gray-900">
-                  {offeredServices[0].duration}
-                </span>
-                days
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <ReactMapGL
@@ -80,7 +93,30 @@ const MapView: FC<MapViewProps> = ({
         {...viewport}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-      ></ReactMapGL>
+      >
+        {offeredServices.map((offeredService) => (
+          <>
+            <Marker
+              key={offeredService.id}
+              latitude={offeredService.paperMaker.lat}
+              longitude={offeredService.paperMaker.long}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 fill-current text-blue-500 cursor-pointer"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Marker>
+          </>
+        ))}
+      </ReactMapGL>
     </div>
   )
 }
