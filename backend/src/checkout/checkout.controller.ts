f8a5816d6afc1f81f508bccb96331213dbfd770e
dangvuthang <dangvuthang97@gmail.com>
2022-08-08 20:50:20 +0700

    update checkout endpoint

--- backend/src/checkout/checkout.controller.ts
+++ backend/src/checkout/checkout.controller.ts
@@ -1,19 +1,31 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from 'src/decorator/user.decorator';
 import { AuthGuard } from 'src/guard/auth.guard';
+import { StripeService } from 'src/stripe/stripe.service';
 import { CheckoutService } from './checkout.service';
 import { CheckoutDto } from './dto/checkout.dto';
 
 @Controller('/checkout')
 export class CheckoutController {
-  constructor(private readonly checkoutService: CheckoutService) {}
+  constructor(
+    private readonly checkoutService: CheckoutService,
+    private readonly stripeService: StripeService,
+  ) {}
   @Post('/')
   @UseGuards(AuthGuard)
   async getCheckoutLink(@User() user, @Body() checkoutDto: CheckoutDto) {
-    const session = await this.checkoutService.createStripeLink(
-      user,
-      checkoutDto,
-    );
-    return { status: 'success', session: session.url };
+    return this.checkoutService.createStripeLink(user, checkoutDto);
+  }
+
+  @Post('/webhook')
+  async handleSuccessfulPayment(@Request() req) {
+    return this.checkoutService.handleSuccessfulPayment(req);
   }
 }
--- backend/src/checkout/checkout.service.ts
+++ backend/src/checkout/checkout.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { StripeService } from 'src/stripe/stripe.service';
 import { CheckoutDto } from './dto/checkout.dto';
@@ -12,28 +13,63 @@ export class CheckoutService {
   ) {}
   async createStripeLink(user: any, checkoutDto: CheckoutDto) {
     const { amount, description, id, name } = checkoutDto;
-    const session = await this.stripeService
-      .getStripe()
-      .checkout.sessions.create({
-        client_reference_id: `${user.id}|${id}`,
-        customer_email: user.username,
-        line_items: [
-          {
-            quantity: 1,
-            price_data: {
-              currency: 'USD',
-              unit_amount: amount * 100,
-              product_data: {
-                name,
-                description,
+    try {
+      const session = await this.stripeService
+        .getStripe()
+        .checkout.sessions.create({
+          client_reference_id: `${user.id}||${id}`,
+          customer_email: user.username,
+          line_items: [
+            {
+              quantity: 1,
+              price_data: {
+                currency: 'USD',
+                unit_amount: amount * 100,
+                product_data: {
+                  name,
+                  description,
+                },
               },
             },
+          ],
+          mode: 'payment',
+          success_url: `${this.configService.get('CLIENT_URL')}/?success=true`,
+          cancel_url: `${this.configService.get('CLIENT_URL')}/?canceled=true`,
+        });
+      return { status: 'success', session: session.url };
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
+  async handleSuccessfulPayment(req: Request) {
+    const sig = req.headers['stripe-signature'];
+    const endpointSecret =
+      'whsec_37318baed2c895221c76ccb4a4e650cf68f1c3b2bbade2362dac03dc893f3cdf';
+    try {
+      const event = this.stripeService
+        .getStripe()
+        .webhooks.constructEvent(req.body, sig, endpointSecret);
+      if (event.type === 'checkout.session.completed') {
+        const sessionInfo = event.data.object as any;
+        const [userId, offeredServiceId] = (
+          sessionInfo.client_reference_id as string
+        ).split('||');
+        const payAmount = sessionInfo.amount_total / 100;
+        const data = await this.prismaService.booking.create({
+          data: {
+            userId,
+            offeredServiceId,
+            payAmount,
+            note: 'ABC',
           },
-        ],
-        mode: 'payment',
-        success_url: `${this.configService.get('CLIENT_URL')}/?success=true`,
-        cancel_url: `${this.configService.get('CLIENT_URL')}/?canceled=true`,
+        });
+        return { status: 'success', data };
+      }
+    } catch (error) {
+      throw new BadRequestException({
+        status: 'error',
+        message: 'Something went wrong',
       });
-    return session;
+    }
   }
 }
