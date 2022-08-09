cfc18553e7972710fc463eae082ff329cb7e8efb
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-08-10 00:18:29 +0700

    add type

--- frontend/components/PapermakerRegisterForm.tsx
+++ frontend/components/PapermakerRegisterForm.tsx
@@ -95,7 +95,7 @@ const PapermakerRegisterForm = () => {
                       apiKey={process.env.GG_API_KEY}
                       placeholder="Enter address"
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      onPlaceSelected={(place) => {
+                      onPlaceSelected={(place: any) => {
                         console.log(JSON.stringify(place?.geometry?.location))
                       }}
                       options={{
