a696ee953577741aa0a3d420d415286cd3885cea
NguyenDangLamPhuong <67723724+NguyenDangLamPhuong@users.noreply.github.com>
2022-08-09 21:51:40 +0700

    using library for validation

--- frontend/components/PapermakerRegisterForm.tsx
+++ frontend/components/PapermakerRegisterForm.tsx
@@ -1,45 +1,35 @@
 import { useForm } from "react-hook-form"
+import Autocomplete from "react-google-autocomplete"
+import PhoneInput from "react-phone-input-2"
+import "react-phone-input-2/lib/style.css"
+import React, { useState } from "react"
 
 const PapermakerRegisterForm = () => {
+  const [selectedImage, setSelectedImage] = useState(null)
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm()
   const onSubmit = (data: any) => console.log(data)
-  // const ref = react.useRef();
-  //     const clear = () => {
-  //         ref.current.value = "";
-  //     };
-  //     const handleImageOnChange = (e: any) => {
-  //         e.preventDefault();
-  //         const file = e.target.files[0];
-  //         if (!file) return setImage(null);
-  //         console.log(file.size);
-  //         if (file.size > 2097152) {
-  //             alert("File is too big!");
-  //             clear();
-  //         } else {
-  //             setImage(e.target.files[0]);
-  //         }
-  //     };
+
   return (
-    <div className="min-h-full flex">
-      <div className="hidden lg:block relative w-0 flex-1">
+    <div className=" w-screen min-h-screen bg-gradient-to-bl from-test via-sky-400 to-test pt-8 px-4 md:pt-30 ">
+      {/* <div className="hidden lg:block relative w-0 flex-1">
         <img
           className="absolute inset-0 h-full w-full object-cover"
           src="signup_img.png"
           alt=""
         />
-      </div>
+      </div> */}
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-        <div className="mx-auto w-full max-w-sm lg:w-96">
+        <div className="container max-w-2xl bg-white mx-auto px-6 py-4 md:px-12 md:py-8 rounded-lg shadow-md">
           <div>
-            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+            <h2 className="text-center mt-6 text-3xl font-extrabold text-gray-900">
               Become a papermaker
             </h2>
           </div>
-          <div className="mt-8">
+          <div>
             <div className="mt-6">
               <form
                 action="#"
@@ -56,12 +46,14 @@ const PapermakerRegisterForm = () => {
                   </label>
                   <div className="mt-1">
                     <input
-                      {...register("email", {required: true ,
+                      {...register("email", {
+                        required: true,
                         pattern:
                           /^([a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$)$/,
                       })}
                       name="email"
                       type="text"
+                      required
                       placeholder="Enter your email"
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
@@ -77,12 +69,14 @@ const PapermakerRegisterForm = () => {
                   </label>
                   <div className="mt-1">
                     <input
-                      {...register("name", {required: true ,
-                        pattern: /[^a-zA-Z]/,
+                      {...register("name", {
+                        required: true,
+                        pattern: /^[a-zA-Z\s]*$/,
                       })}
                       name="name"
                       type="text"
                       placeholder="Enter your name"
+                      required
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                     {errors.name && (
@@ -96,15 +90,18 @@ const PapermakerRegisterForm = () => {
                     Address
                   </label>
                   <div className="mt-1">
-                    <input
-                      type="text"
-                      placeholder="Enter your address"
-                      {...register("address", { required: true })}
+                    <Autocomplete
+                      aria-required
+                      apiKey={"AIzaSyBaWeEkmnPVpBj8K4oflGBlQdnS54i1OH4"}
+                      placeholder="Enter address"
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      onPlaceSelected={(place) => {
+                        console.log(JSON.stringify(place?.geometry?.location))
+                      }}
+                      options={{
+                        types: ["geocode", "establishment"],
+                      }}
                     />
-                    {errors.address && (
-                      <p className="text-red-500 text-sm">Empty Address!</p>
-                    )}
                   </div>
                   <label
                     htmlFor="phone"
@@ -113,20 +110,13 @@ const PapermakerRegisterForm = () => {
                     Phone Number
                   </label>
                   <div className="mt-1">
-                    <input
-                      {...register("phoneNumber", {required: true ,
-                        pattern: /^(\s*|\d+)$/,
-                      })}
-                      type="phone"
-                      placeholder="Enter your phone number"
-                      autoComplete="phone"
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    <PhoneInput
+                      inputProps={{
+                        name: "phone",
+                        required: true,
+                        autoFocus: true,
+                      }}
                     />
-                    {errors.phoneNumber && (
-                      <p className="text-red-500 text-sm">
-                        Invalid Phone Number!
-                      </p>
-                    )}
                   </div>
                   <label
                     htmlFor="password"
@@ -147,7 +137,6 @@ const PapermakerRegisterForm = () => {
                       <p className="text-red-500 text-sm">Empty Password!</p>
                     )}
                   </div>
-
                   <label
                     htmlFor="password"
                     className="block text-sm font-medium text-gray-700"
@@ -158,6 +147,7 @@ const PapermakerRegisterForm = () => {
                     <input
                       {...register("confirmPassword", { required: true })}
                       required
+                      type={"password"}
                       placeholder="*******"
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
@@ -170,26 +160,44 @@ const PapermakerRegisterForm = () => {
                   <label className="block text-sm font-medium text-gray-700">
                     Photo
                   </label>
-                  <div className="mt-1 flex items-center">
+                  <div className="mt-1 mb-12 flex items-center">
                     <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                      <svg
-                        className="h-full w-full text-gray-300"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                      </svg>
+                      {selectedImage && (
+                        <div>
+                          <img
+                            alt="not found"
+                            width={"250px"}
+                            src={URL.createObjectURL(selectedImage)}
+                          />
+                        </div>
+                      )}
+                      {!selectedImage && (
+                        <div>
+                          <svg
+                            className="h-full w-full text-gray-300"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                          </svg>
+                        </div>
+                      )}
                     </span>
-                    <button
-                      type="button"
-                      className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Upload
-                    </button>
+                    <input
+                      type="file"
+                      name="myImage"
+                      className="ml-5 w-full bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      onChange={(event: any) => {
+                        console.log(event.target.files[0])
+                        setSelectedImage(event.target.files[0])
+                      }}
+                    />
                   </div>
+                </div>
+                <div>
                   <input
                     type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                   />
                 </div>
               </form>
