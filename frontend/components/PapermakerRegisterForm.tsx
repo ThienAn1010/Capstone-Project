8480a9334d7fbe6927f9c93ad728cf8a17cfd43a
NguyenDangLamPhuong <67723724+NguyenDangLamPhuong@users.noreply.github.com>
2022-08-09 22:06:51 +0700

    update

--- frontend/components/PapermakerRegisterForm.tsx
+++ frontend/components/PapermakerRegisterForm.tsx
@@ -92,7 +92,7 @@ const PapermakerRegisterForm = () => {
                   <div className="mt-1">
                     <Autocomplete
                       aria-required
-                      apiKey={"AIzaSyBaWeEkmnPVpBj8K4oflGBlQdnS54i1OH4"}
+                      apiKey={process.env.GG_API_KEY}
                       placeholder="Enter address"
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       onPlaceSelected={(place) => {
