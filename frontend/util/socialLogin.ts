e5e051e1aff4531d2aab64f5a2285a57953dd48c
dangvuthang <dangvuthang97@gmail.com>
2022-08-09 17:42:36 +0700

    fix wrong redirect url

--- frontend/util/socialLogin.ts
+++ frontend/util/socialLogin.ts
@@ -2,7 +2,7 @@ const getFacebookOAuthUrl = () => {
   const rootUrl = "https://www.facebook.com/v14.0/dialog/oauth"
   const options = {
     client_id: "945889392770900",
-    redirect_uri: `${process.env.NEXT_PUBLIC_BACKEND_API_DEVELOPMENT}/`,
+    redirect_uri: `${process.env.NEXT_PUBLIC_CLIENT_CALLBACK}/`,
     scope: ["public_profile", "email"].join(","),
   }
   const queryString = new URLSearchParams(options)
@@ -14,7 +14,7 @@ const getGoogleOAuthUrl = () => {
   const options = {
     client_id:
       "346121408906-evbb0dn7ncjb37hcjjrtl687o8u0jq7u.apps.googleusercontent.com",
-    redirect_uri: `${process.env.NEXT_PUBLIC_BACKEND_API_DEVELOPMENT}/`,
+    redirect_uri: `${process.env.NEXT_PUBLIC_CLIENT_CALLBACK}/`,
     response_type: "code",
     scope: [
       "https://www.googleapis.com/auth/userinfo.profile",
