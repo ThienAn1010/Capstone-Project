f4f03a9490e2a9326e0e9ff5b0139e8027e73616
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-08-11 02:02:21 +0700

    try suspense & lazy import

--- frontend/pages/service/[id]/checkout/index.tsx
+++ frontend/pages/service/[id]/checkout/index.tsx
@@ -1,40 +1,49 @@
 import type { NextPage } from "next"
 import { useRouter } from "next/router"
-import CheckOutForm from "../../../../components/CheckOut/CheckOutForm"
-import CheckOutSummary from "../../../../components/CheckOut/CheckOutSummary"
+import React, { Suspense } from "react"
 import CheckOutLoader from "../../../../components/LoadingSkeleton/checkOutLoader"
 import useGetMe from "../../../../hooks/useGetMe"
 import useGetOfferedService from "../../../../hooks/useGetOfferedService"
 
+const CheckOutForm = React.lazy(
+  () => import("../../../../components/CheckOut/CheckOutForm")
+)
+const CheckOutSummary = React.lazy(
+  () => import("../../../../components/CheckOut/CheckOutSummary")
+)
+
 const Checkout: NextPage = () => {
-  const { data: userData, isLoading: userLoading } = useGetMe()
   const router = useRouter()
+  const id = router.query.id
+  const { data: userData, isLoading: userLoading } = useGetMe()
   const { data: serviceData, isLoading: serviceLoading } = useGetOfferedService(
-    router.query.id as string
+    id as string
   )
   const isLoading = userLoading || serviceLoading
   return (
     <>
-      {isLoading ? (
-        <CheckOutLoader />
-      ) : (
-        <>
-          <main className="bg-checkout">
-            <div className="max-w-7xl mx-auto pt-14 pb-24 px-4 sm:px-6 lg:px-8">
-              <div className="max-w-2xl mx-auto lg:max-w-none">
-                <h1 className="sr-only">Checkout</h1>
-                <form
-                  id="checkout"
-                  className="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16"
-                >
-                  <CheckOutForm userData={userData} />
-                  <CheckOutSummary serviceData={serviceData} />
-                </form>
+      <Suspense fallback={<CheckOutLoader />}>
+        {isLoading ? (
+          <CheckOutLoader />
+        ) : (
+          <>
+            <main className="bg-checkout">
+              <div className="max-w-7xl mx-auto pt-14 pb-24 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-2xl mx-auto lg:max-w-none">
+                  <h1 className="sr-only">Checkout</h1>
+                  <form
+                    id="checkout"
+                    className="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16"
+                  >
+                    <CheckOutForm userData={userData} />
+                    <CheckOutSummary serviceData={serviceData} />
+                  </form>
+                </div>
               </div>
-            </div>
-          </main>
-        </>
-      )}
+            </main>
+          </>
+        )}
+      </Suspense>
     </>
   )
 }
