c4750c66968b9101898741d19993a7e7646b78c9
dangvuthang <dangvuthang97@gmail.com>
2022-08-14 23:23:06 +0700

    add sendgrid service

---
+++ backend/src/sendgrid/sendgrid.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as sgMail from '@sendgrid/mail';
+@Injectable()
+export class SendGridService {
+  constructor(private configService: ConfigService) {}
+  private readonly sendgrid = sgMail.setApiKey(
+    this.configService.get('SENDGRID_API_KEY'),
+  );
+  getSendGrid() {
+    return this.sendgrid;
+  }
+}
