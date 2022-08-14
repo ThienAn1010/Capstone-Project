4868b15bf85d51ca1b49fdb6389ea6e79311bd09
dangvuthang <dangvuthang97@gmail.com>
2022-08-14 23:24:13 +0700

    add route to handle get all logged in user booking and update user info

--- backend/src/user/users.service.ts
+++ backend/src/user/users.service.ts
@@ -6,6 +6,7 @@ import {
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserUpdateDto } from './dto/user-update-service.dto';
 
 @Injectable()
 export class UsersService {
@@ -13,6 +14,21 @@ export class UsersService {
     private readonly configService: ConfigService,
     private readonly prismaService: PrismaService,
   ) {}
+  private async createToken(id: string) {
+    return new Promise((res, rej) => {
+      jwt.sign(
+        { userId: id },
+        this.configService.get('JWT_SECRET'),
+        {
+          expiresIn: '1d',
+        },
+        (err, decoded) => {
+          if (err) rej(err);
+          res(decoded);
+        },
+      );
+    });
+  }
   private async verifyToken(token: string) {
     if (!token) throw new BadRequestException();
     return new Promise((res, rej) => {
@@ -53,5 +69,54 @@ export class UsersService {
     if (typeof userId !== 'string') throw new BadRequestException();
     const user = await this.findUser(userId);
     return user;
+  }
+
+  async getAllMyBookings(userId: string) {
+    const bookings = await this.prismaService.booking.findMany({
+      where: {
+        userId,
+      },
+      select: {
+        id: true,
+        note: true,
+        status: true,
+        payAmount: true,
+        offeredService: {
+          include: {
+            paperMaker: {
+              include: {
+                user: {
+                  select: {
+                    id: true,
+                    username: true,
+                    name: true,
+                    address: true,
+                    lat: true,
+                    long: true,
+                    picture: true,
+                    phoneNumber: true,
+                    createdAt: true,
+                    role: true,
+                  },
+                },
+              },
+            },
+            service: true,
+          },
+        },
+      },
+    });
+    return { status: 'success', length: bookings.length, bookings };
+  }
+
+  async updateMe(userId: string, userUpdateDto: UserUpdateDto) {
+    const updatedMe = await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: userUpdateDto,
+    });
+    const accessToken = await this.createToken(updatedMe.id);
+    return accessToken;
   }
 }
