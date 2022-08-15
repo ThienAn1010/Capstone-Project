d0c881f3e876a2525a268f4e44110c77217d5b33
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-08-16 01:38:30 +0700

    update user dashboard

--- frontend/components/DashBoard/ProfileForm.tsx
+++ frontend/components/DashBoard/ProfileForm.tsx
@@ -91,6 +91,70 @@ export default function CheckOutForm() {
         <div className="mt-6 flex flex-col lg:flex-row">
           <div className="flex-grow space-y-6">
             <div>
+              <p
+                className="text-sm font-medium text-gray-700"
+                aria-hidden="true"
+              >
+                Photo
+              </p>
+              <div className="mt-1 lg:hidden">
+                <div className="flex items-center">
+                  <div
+                    className="flex-shrink-0 inline-block rounded-full overflow-hidden h-12 w-12"
+                    aria-hidden="true"
+                  >
+                    <img
+                      className="rounded-full h-full w-full"
+                      src={userData?.picture}
+                      alt="Avatar"
+                    />
+                  </div>
+                  <div className="ml-5 rounded-md shadow-sm">
+                    <div className="group relative border border-gray-300 rounded-md py-2 px-3 flex items-center justify-center hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-sky-500">
+                      <label
+                        htmlFor="mobile-user-photo"
+                        className="relative text-sm leading-4 font-medium text-gray-700 pointer-events-none"
+                      >
+                        <span>Change</span>
+                        <span className="sr-only">user photo</span>
+                      </label>
+                      <input
+                        id="mobile-user-photo"
+                        name="user-photo"
+                        type="file"
+                        className="absolute w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
+                        onChange={(event: any) => {
+                          console.log(event.target.files[0])
+                          setSelectedImage(event.target.files[0])
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="hidden relative w-40 h-40 ml-4 rounded-full overflow-hidden lg:block">
+                <img
+                  className="rounded-full w-40 h-40"
+                  src={userData?.picture}
+                  alt=""
+                />
+                <label
+                  htmlFor="desktop-user-photo"
+                  className="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm font-medium text-white opacity-0 hover:opacity-100 focus-within:opacity-100"
+                >
+                  <span>Change</span>
+                  <span className="sr-only">user photo</span>
+                  <input
+                    type="file"
+                    id="desktop-user-photo"
+                    name="user-photo"
+                    className="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
+                  />
+                </label>
+              </div>
+            </div>
+            <div>
               <label
                 htmlFor="username"
                 className="block text-sm font-medium text-gray-700"
@@ -99,8 +163,7 @@ export default function CheckOutForm() {
               </label>
               <div className="mt-1 rounded-md shadow-sm flex">
                 <span className="bg-slate-50 border border-r-0 capitalize border-slate-300 rounded-l-md px-3 inline-flex items-center text-slate-500 sm:text-sm">
-                  {/* {data?.role} */}
-                  User
+                  {userData?.role}
                 </span>
                 <input
                   type="text"
@@ -112,88 +175,6 @@ export default function CheckOutForm() {
                   defaultValue={userData?.username}
                 />
               </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="about"
-                className="block text-sm font-medium text-gray-700"
-              >
-                About
-              </label>
-              <div className="mt-1">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="shadow-sm focus:ring-sky-500 focus:border-sky-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                />
-              </div>
-              <p className="mt-2 text-sm text-gray-500">
-                Brief description for your profile. URLs are hyperlinked.
-              </p>
-            </div>
-          </div>
-
-          <div className="mt-6 flex-grow lg:mt-0 lg:ml-6 lg:flex-grow-0 lg:flex-shrink-0">
-            <p className="text-sm font-medium text-gray-700" aria-hidden="true">
-              Photo
-            </p>
-            <div className="mt-1 lg:hidden">
-              <div className="flex items-center">
-                <div
-                  className="flex-shrink-0 inline-block rounded-full overflow-hidden h-12 w-12"
-                  aria-hidden="true"
-                >
-                  <img
-                    className="rounded-full h-full w-full"
-                    src={userData?.picture}
-                    alt="Avatar"
-                  />
-                </div>
-                <div className="ml-5 rounded-md shadow-sm">
-                  <div className="group relative border border-gray-300 rounded-md py-2 px-3 flex items-center justify-center hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-sky-500">
-                    <label
-                      htmlFor="mobile-user-photo"
-                      className="relative text-sm leading-4 font-medium text-gray-700 pointer-events-none"
-                    >
-                      <span>Change</span>
-                      <span className="sr-only">user photo</span>
-                    </label>
-                    <input
-                      id="mobile-user-photo"
-                      name="user-photo"
-                      type="file"
-                      className="absolute w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
-                      onChange={(event: any) => {
-                        console.log(event.target.files[0])
-                        setSelectedImage(event.target.files[0])
-                      }}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="hidden relative rounded-full overflow-hidden lg:block">
-              <img
-                className="relative rounded-full w-40 h-40"
-                src={userData?.picture}
-                alt=""
-              />
-              <label
-                htmlFor="desktop-user-photo"
-                className="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm font-medium text-white opacity-0 hover:opacity-100 focus-within:opacity-100"
-              >
-                <span>Change</span>
-                <span className="sr-only">user photo</span>
-                <input
-                  type="file"
-                  id="desktop-user-photo"
-                  name="user-photo"
-                  className="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
-                />
-              </label>
             </div>
           </div>
         </div>
@@ -212,7 +193,7 @@ export default function CheckOutForm() {
               })}
               type="text"
               name="name"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+              className="block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
               defaultValue={userData?.name}
             />
             {errors.name && (
