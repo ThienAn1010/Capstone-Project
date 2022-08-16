d1367afa256d4e026342b7232a7a2e9623d7bd1f
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2022-08-16 20:50:01 +0700

    color change

--- frontend/pages/profile/[id].tsx
+++ frontend/pages/profile/[id].tsx
@@ -70,7 +70,7 @@ const Profile: NextPage = () => {
                                 key={item.name}
                                 className={classNames(
                                   item.current
-                                    ? "bg-blue-50 border-blue-500 text-blue-700 hover:bg-teal-50 hover:text-blue-700"
+                                    ? "bg-blue-50 border-blue-500 text-blue-700 hover:bg-blue-50 hover:text-blue-700"
                                     : "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900",
                                   "group border-l-4 px-3 py-2 flex items-center text-sm font-medium hover:cursor-pointer"
                                 )}
@@ -111,7 +111,7 @@ const Profile: NextPage = () => {
                                 key={item.name}
                                 className={classNames(
                                   item.current
-                                    ? "bg-teal-50 border-teal-500 text-teal-700 hover:bg-teal-50 hover:text-teal-700"
+                                    ? "bg-blue-50 border-blue-500 text-blue-700 hover:bg-blue-50 hover:text-blue-700"
                                     : "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900",
                                   "group border-l-4 px-3 py-2 flex items-center text-sm font-medium hover:cursor-pointer"
                                 )}
@@ -135,7 +135,7 @@ const Profile: NextPage = () => {
                                 <item.icon
                                   className={classNames(
                                     item.current
-                                      ? "text-teal-500 group-hover:text-teal-500"
+                                      ? "text-blue-500 group-hover:text-blue-500"
                                       : "text-gray-400 group-hover:text-gray-500",
                                     "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
                                   )}
