6b497f8b67249c46b6f9ef5d063a38a555476b44
dangvuthang <dangvuthang97@gmail.com>
2022-08-19 18:31:25 +0700

    add route to handle booking

--- backend/src/booking/booking.controller.ts
+++ backend/src/booking/booking.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
 import { User } from 'src/decorator/user.decorator';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { RoleGuard } from 'src/guard/role.guard';
 import { BookingService } from './booking.service';
+import { UpdatedBookingDto } from './dto/updated-booking.dto';
 
 @Controller('/bookings')
 export class BookingController {
@@ -10,5 +11,18 @@ export class BookingController {
   @Get('')
   async getAllbookings() {
     return this.bookingService.getAllBookings();
+  }
+
+  @Get('/:bookingId')
+  async getDetailBooking(@Param('bookingId') bookingId: string) {
+    return this.bookingService.getBooking(bookingId);
+  }
+
+  @Patch('/:bookingId')
+  async updateBooking(
+    @Param('bookingId') bookingId: string,
+    @Body() updatedBookingDto: UpdatedBookingDto,
+  ) {
+    return this.bookingService.updateBooking(bookingId, updatedBookingDto);
   }
 }
