cb718ada5228096f3151663d423571b3a91b6026
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2022-09-05 18:51:39 +0700

    show fake location of marker

--- frontend/components/Map/Map.tsx
+++ frontend/components/Map/Map.tsx
@@ -1,8 +1,7 @@
 import React from "react"
 import { useRouter } from "next/router"
-import { GoogleMap, useLoadScript} from "@react-google-maps/api"
+import { GoogleMap, useLoadScript, MarkerF, InfoWindowF} from "@react-google-maps/api"
 import useGetOfferedService from "../../hooks/useGetOfferedService"
-import {MarkerF} from '@react-google-maps/api'
 const mapContainerStyle = {
   height: "70vh",
 }
@@ -11,6 +10,7 @@ export default function Map() {
   const router = useRouter()
   const { data } = useGetOfferedService(router.query.id as string)
   const [markers] = React.useState([{}])
+  const [selected, setSelected] = React.useState(null);
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GG_API_KEY!,
@@ -22,32 +22,50 @@ export default function Map() {
   const onLoad = (marker: any) => {
     console.log('marker: ', marker)
   }
+  // const mapRef = React.useRef();
+  // const onMapLoad = React.useCallback((map:undefined) => {
+  //   mapRef.current = map;
+  // }, []);
   console.log(data)
   if (!isLoaded)
     return <div className="text-center mt-5 text-3xl">Loading ...</div>
   return (
-    <div className="mt-10 border-4 border-orange-300 hover:border-indigo-400">
-      <div>
-        <GoogleMap
+    <div>
+      <div className="mt-10 border-4 border-orange-300 hover:border-indigo-400">
+        <div>
+          <GoogleMap
             zoom={20}
             center={center}
             mapContainerStyle={mapContainerStyle}
             mapContainerClassName="map-container"
+            onLoad={onLoad}
             options={{
                 disableDefaultUI: true,
                 zoomControl: true,
                 fullscreenControl: true
             }}
-            onLoad={onLoad}
-        >
-            {markers.map((marker: any) => (
-                <MarkerF
+          >
+            {markers.map((marker:any) => (
+              <MarkerF
                 key={`${marker.lat}-${marker.lng}`}
                 position={center}
-                />
+                onClick={() => {
+                  setSelected(marker);
+                }}
+              />
             ))}
-        </GoogleMap>
+            {selected ? (
+              <InfoWindowF
+                position={center}
+                onCloseClick={() => setSelected(null)}
+              >
+                <div>{data!.paperMaker.user.address!}</div>
+              </InfoWindowF>
+            ) : null}
+          </GoogleMap>
+        </div>
       </div>
+      <div className="text-center mt-6">{data!.paperMaker.user.address!}</div>
     </div>
   )
 }
