17c098995c5d8fe65ff1c629a4a0bd0755630ef3
dangvuthang <dangvuthang97@gmail.com>
2022-09-08 23:19:56 +0700

    update send email

--- backend/src/booking/booking.module.ts
+++ backend/src/booking/booking.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SendGridModule } from 'src/sendgrid/sengrid.module';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
   controllers: [BookingController],
   providers: [BookingService],
+  imports: [SendGridModule],
 })
 export class BookingModule {}
--- backend/src/booking/booking.service.ts
+++ backend/src/booking/booking.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as dayjs from 'dayjs';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SendGridService } from 'src/sendgrid/sendgrid.service';
 import { UpdatedBookingDto } from './dto/updated-booking.dto';
 
 const select = {
@@ -41,7 +44,11 @@ const select = {
 
 @Injectable()
 export class BookingService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly sendGridService: SendGridService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async getAllBookings() {
     const bookings = await this.prismaService.booking.findMany({
@@ -86,12 +93,111 @@ export class BookingService {
       },
       select,
     });
+
     if (!booking) {
       throw new NotFoundException({
         status: 'fail',
         message: 'No booking found with a given id',
       });
     }
+
+    if (!updatedBookingDto.isFinishedConfirmed) {
+      if (booking.status === 'accept') {
+        const sendToUser = {
+          to: booking.user.username, // Change to your recipient
+          from: {
+            email: this.configService.get('SENDGRID_VERIFIED_SENDER'),
+            name: 'Paperwork',
+          }, // Change to your verified sender
+          subject: `Your booking has been accepted`,
+          html: `
+          <p>Dear ${booking.user.name}</h1>
+          <p>Your boooking has been accepted by ${
+            booking.offeredService.paperMaker.user.name
+          }. You can keep track of the progress in the dashboard. The paperwork is expected to take <strong>${
+            booking.offeredService.duration
+          }</strong> days, and will finish in <strong>${dayjs(
+            booking.acceptedAt,
+          )
+            .add(booking.offeredService.duration)
+            .format('DD-MM-YYYY')}</strong></p>
+          <p>If you have any problem. Please don't hesitate to contact us</p>
+          <p>Thank you for using our service.</p>
+          `,
+        };
+        await this.sendGridService.getSendGrid().send(sendToUser);
+      }
+
+      if (booking.status === 'deny') {
+        const sendToUser = {
+          to: booking.user.username, // Change to your recipient
+          from: {
+            email: this.configService.get('SENDGRID_VERIFIED_SENDER'),
+            name: 'Paperwork',
+          }, // Change to your verified sender
+          subject: `Your booking has been denied`,
+          html: `
+          <p>Dear ${booking.user.name}</h1>
+          <p>Your boooking has been denied by ${booking.offeredService.paperMaker.user.name}. Your money has been refunded accordingly</p>
+          <p>If you have any problem. Please don't hesitate to contact us</p>
+          <p>Thank you for using our service.</p>
+          `,
+        };
+        await this.sendGridService.getSendGrid().send(sendToUser);
+      }
+
+      if (booking.status === 'drop') {
+        const sendToUser = {
+          to: booking.user.username, // Change to your recipient
+          from: {
+            email: this.configService.get('SENDGRID_VERIFIED_SENDER'),
+            name: 'Paperwork',
+          }, // Change to your verified sender
+          subject: `You have dropped your booking`,
+          html: `
+          <p>Dear ${booking.user.name}</h1>
+          <p>You have dropped our service, and we will contact you to learn more about your service before refundeding</p>
+          <p>If you have any problem. Please don't hesitate to contact us</p>
+          <p>Thank you for using our service.</p>
+          `,
+        };
+        const sendToPapermaker = {
+          to: booking.offeredService.paperMaker.user.username, // Change to your recipient
+          from: {
+            email: this.configService.get('SENDGRID_VERIFIED_SENDER'),
+            name: 'Paperwork',
+          }, // Change to your verified sender
+          subject: `The customer has dropped your booking`,
+          html: `
+          <p>Dear ${booking.offeredService.paperMaker.user.username}</h1>
+          <p>The customer has dropped your booing, and we will contact you to learn more about the booking</p>
+          <p>If you have any problem. Please don't hesitate to contact us</p>
+          <p>Thank you for using our service.</p>
+          `,
+        };
+        await this.sendGridService.getSendGrid().send(sendToUser);
+        await this.sendGridService.getSendGrid().send(sendToPapermaker);
+      }
+
+      if (booking.status === 'success') {
+        const sendToUser = {
+          to: booking.user.username, // Change to your recipient
+          from: {
+            email: this.configService.get('SENDGRID_VERIFIED_SENDER'),
+            name: 'Paperwork',
+          }, // Change to your verified sender
+          subject: `Your booking has been completed`,
+          html: `
+          <p>Dear ${booking.user.name}</h1>
+          <p>Your boooking has been completed. Please confirm it in your dashboard to finish the process.</p>
+          <p>If you have any problem. Please don't hesitate to contact us</p>
+          <p>Thank you for using our service.</p>
+          `,
+        };
+        await this.sendGridService.getSendGrid().send(sendToUser);
+      }
+    }
+
     return { status: 'success', booking: booking };
   }
 }
